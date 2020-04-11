43ad7db2e55b05303726a5f4f27388285fc7b32e
ShadowOfUchiha <johnnymihai33@gmail.com>
2020-04-11 13:41:20 +0200

    Update
    removed 2 webpages and re-worked the portfolio pages

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {stickyNav()};
+        
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
+        
+function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+} // sticky navbar
